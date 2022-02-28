20a321b0034ad0df0258ef5304998e76dca00d3a
adam-dabet <adamdabet@gmail.com>
2022-02-28 15:15:17 -0800

    correct

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -6,7 +6,7 @@ public class SkillDemoTest {
 
     @Test
     public void Addition(){
-        assertEquals(10, SkillDemo.add(5, 10));
+        assertEquals(10, SkillDemo.add(5, 5));
     }
     
 }
