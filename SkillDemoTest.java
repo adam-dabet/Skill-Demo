2e06aac93a7307f232a7dc31212d9fdb39270c3e
adam-dabet <adamdabet@gmail.com>
2022-02-28 15:14:51 -0800

    failing tests

---
+++ SkillDemoTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+
+public class SkillDemoTest {
+
+    @Test
+    public void Addition(){
+        assertEquals(10, SkillDemo.add(5, 10));
+    }
+    
+}
